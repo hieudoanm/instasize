9650e8eedb22308a555bb7b35cf39b931161375b
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-12 20:53:35 +0700

    add cap

--- docs/_next/static/chunks/f48f7bacb96d2a13.js
+++ docs/_next/static/chunks/f48f7bacb96d2a13.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/757701caddb2a196.js",
   "static/chunks/83327fde76fafc62.js",
   "static/chunks/8da08e0e7b713d54.js",
-  "static/chunks/afd16ad20dbcde3e.css",
+  "static/chunks/a636394c25464673.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-194b9538c3a65795.js"
+  "static/chunks/turbopack-e6e0dd6546adb48e.js"
 ])
--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.github.hieudoanm.ig',
-  appName: 'Instagram',
+  appName: 'IG',
   webDir: '../../docs',
   android: { path: 'mobile/android' },
   ios: { path: 'mobile/ios' },
