7ea8b58e6ea2059f576a269d8ee8a3a6d7cc0854
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-25 21:23:58 +0700

    Use HeadTemplate

--- docs/_next/static/chunks/0lasly7-1_iv..js
+++ docs/_next/static/chunks/0lasly7-1_iv..js
@@ -1,8 +1,9 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/09.ti1~fhz7y8.js",
+  "static/chunks/17p_gj8cb5.4y.js",
+  "static/chunks/0xqm-x93dplc8.js",
   "static/chunks/00hcf8ri3x~-w.js",
   "static/chunks/13cjkb0_ea4fr.js",
   "static/chunks/01l9j8fsglyr8.css",
   "static/chunks/0p44cybxe7kuv.css",
-  "static/chunks/turbopack-06wstfsd87h_5.js"
+  "static/chunks/turbopack-02et~-z11tiq1.js"
 ])
